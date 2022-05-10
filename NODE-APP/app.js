a8a0c4e797e54f95e87850c951d36bb6690aae0b
T1G3R-DEV <daniel04mik@gmail.com>
2022-05-10 18:19:39 +0200

    RAW node app first working version

---
+++ NODE-APP/app.js
@@ -0,0 +1,45 @@
+//
+//
+//    YTNP2txt by Daniel M. (c)2022
+//
+//      https://github.com/T1G3R-DEV/YT2txt
+//
+//  
+//
+
+
+
+const http = require('http');   //lib for webserver
+const url = require('url');     //lib for parsing GET requests
+const fs = require('fs');       //lib for writing to the files
+
+const requestListener = function (req, res) {
+    const queryObject = url.parse(req.url, true).query;
+    console.log(queryObject);
+    if (queryObject['video_title'] !=null)
+    {
+        console.log(queryObject);
+
+
+        fs.writeFile('./video_title.txt', String(queryObject['video_title']), err => {
+            if (err) {
+            console.error(err);
+            }
+            // file written successfully
+        });
+
+        fs.writeFile('./video_creator.txt', String(queryObject['video_creator']), err => {
+            if (err) {
+            console.error(err);
+            }
+            // file written successfully
+        });
+    
+    }
+    res.writeHead(200);
+  
+    res.end('For help visit the github: https://github.com/T1G3R-DEV/YT2txt/ !');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8099);
